38fe4f0197e8bab72c3e128b4f669843cef1f09e
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-11-01 10:09:54 -0400

    build my api in 45 minutes

---
+++ functions/secrets.js
@@ -0,0 +1,12 @@
+export default {
+  "type": "service_account",
+  "project_id": "albums-api-gk",
+  "private_key_id": "13c4a863b04b1d5734aa10ba8ad45366bb341fad",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDcHH1Yc/v8X4lg\n1omGmDgfzymrBH/OGTKUqXB1OthYdaG7luPk4/4M57rd/AVwkcYtZI7X5LrIZw9c\ny2pSlPrOmy8O148BXPsd3NjaKTvFP/IeVSxrV7/UUKEYVuylfnvbTgDCSoZJ9I6d\n03oU2+McFIjU3XRuPYnvywV0fxuJ7xwqzQJ5f9aLPfgTqNIyRqhv6+k7MWlWOvWM\nOx8G4uW+nVRtAjZzC9uwJ2R1NsQjFgAcdeFwPYMurC5Fcs1FUrrT9gA7F0JDlKLX\nOdtgN5p+iZG8Uzuj1tTBkMlRsXCmbk5kUE5OKUL0XSYSU5iW2qUAx37AUT7j10c9\n54iBRpx5AgMBAAECggEALXjJQFO9s/Z8p//KK38Y4SFoB0eHq6ky9NlIPT+p4CR1\nBA75B5k+K6ZXyXI+IXJMEaabh+hd2fbIirAPR/41DierAhLwHST7T8EHH5KzIV3D\nhBaudYKMQ61gimmyu8G0jqq/p2PD+FVIfTxqFRMUCQII/oi9e+TEsUAs4VM8+jKE\nYaCbW6VA2tj0BhQvW6w1hJmEHYHeQtzBbvqKYvQdzYcYHdne7aTP0iZw55PFYMlk\nMJSx6jKn+Onp1j/bmmsVAu74IqnZcdC8fcheHOPFQjHLIpzbrl/UmsYi2U3hInog\nWyAMI4FXSfZtwC6mEmG29rM1NxbNMY3pbX2vqJOgbQKBgQD3kpMxnTe48+lea0Px\naA2Xu3SL17FXfknifUj73OGBa5aLWUnCgG/DzGTrGooI7/doZYLRkOOJVu/WmNOL\n3ASMx9csIqMN9eYpbGh0HXcO8HpxgXvGX0Z5iPRFxv8B1PtqJanwwquWY/BrVOCW\nA+DENN13+Jm/75JQiWsND8RAgwKBgQDjmpvHvewZIHntfyuAfhF8ivY56os/uGIk\nBjEYYII4EQ6odhAik0w3OLW6kZc24zLEreApx2cYWZmwLmB7jJz7UOPyVj8lPWXl\n9dDbZBIhvpiGzMa80AWqADGc++zeyV64M9Oggf1nX3BFTsVtPEkPSMgWbj4BJngS\nwePew5DmUwKBgAUVq5Q/SF+yEr7vsQuKOY0+OlyM54WGgHn4XMFfffosfYELKmqm\nPKVm00PI3cDlPM2WGCO5uBKgCHj91opu0GEb0Z2EcJjezgVXxqVr3YAfDES8hhBC\n8K2aVHoCZkZhW3CIqX7aYK/MCaoKqjoqhjlBZLMMFgxNHVIuIFa6ViuFAoGAB5MM\nniAW7FslU58IzpTjdhaVzSWeDrfPJE3EcoA7tSv0Je0P6mamgav0eEoCE0SKWvtC\nW4GUT74jnDZe5Z2vV7nAbD8siMu7SWTd139EDzVtlAbakkwF9Dtfa08C+gMPkOfA\nojXRPhByBwxxC4qXD9gnOH/gtl8WXeFv+OjfOJsCgYEArL0zdHOBcEfmTqAPrGub\nBNZGPpvAn3qwQ6kwVNcgU1C4KmXPV0qXxePyikwX+ixeFVuiq3HmgxZiWsdQjMk6\nPJY5GtDoN4B4+Zanv8dKEScNmoASxNnrlrJ9M+CDaGwmPYy86oPH8+DlFIBfuONA\nHp4w02fsHTXxrpnYJwIdXOM=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-khwu1@albums-api-gk.iam.gserviceaccount.com",
+  "client_id": "103303924698188301011",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-khwu1%40albums-api-gk.iam.gserviceaccount.com"
+}
